106437c9c3b5ffbf894a36f9b529ba183243fb42
Chris Roth <41457543+TexaCaliCoder@users.noreply.github.com>
2023-10-14 14:19:37 -0500

    Server Validation completed: - Luhn working as expected. - All tests passing -= May refactoradd additonal tests for just the validation function

--- server/server.ts
+++ server/server.ts
@@ -15,16 +15,47 @@ app.get('/', (req: Request, res: Response)=>{
 
 
 app.post('/validateCard', (req: Request, res:Response)=>{
-console.log(req.body)
 const creditCardNumber: string = req.body.creditCardNumber;
 
 if(!creditCardNumber){
   return res.status(400).json({error: 'Please provide a Credit Card Number'})
 }
-// TODO: Add the validation
+if(!isValidLuhn(creditCardNumber)){
+  return res.status(400).json({error: 'Not a valid Credit Card Number'})
+}
+
 return res.status(200).json({msg: 'Success'})
 });
 
+
+const isValidLuhn = (number:string):boolean => {
+  // here we convert the number to an array of numbers for enumeration
+  const digits: number[] = number.split('').map((num)=> parseInt(num, 10))
+
+  // set variables outside the scope of the loop to persist 
+  let sum = 0;
+  let shouldDoubleDigit = false;
+
+  // we start are loop at the end "check digit" and work backwards.
+  for (let i = digits.length - 1; i>=0; i --) {
+    let digit = digits[i];
+
+    //  if we should double the digit we do so. 
+    if(shouldDoubleDigit) {
+      digit *= 2
+      // based on Luhn's alg, if the digit is greater than 9 we need to subtract 9 
+      if(digit >  9){
+        digit -= 9;
+      }
+    }
+    // add the our current working digit or difference to our sum. 
+    sum += digit;
+    // if we doubled the last digit we don't want to double the next. 
+    shouldDoubleDigit = !shouldDoubleDigit
+  }
+  // based on Luhn the sum should be divisible by 10 to be considered valid. 
+  return sum % 10 === 0; 
+}
 
 
 if (process.env.NODE_ENV !== 'test') {
--- server/tests/server.test.ts
+++ server/tests/server.test.ts
@@ -21,10 +21,18 @@ describe('Server Tests', () =>{
         // Check that the response body contains the expected error message.
         expect(response.body).toEqual({ error: 'Please provide a Credit Card Number' });
       });
-    
-      // Test for the POST /validateCard endpoint with a valid credit card number.
-      it('should respond with 200 for POST /validateCard with credit card number', async () => {
+
+      it('should respond with 400 for POST /validateCard without a valid credit card number', async () => {
         const response = await request(server).post('/validateCard').send({ creditCardNumber: '1234567890123456' });
+        // Check that the response status is 400 (Bad Request).
+        expect(response.status).toBe(400);
+        // Check that the response body contains the expected error message.
+        expect(response.body).toEqual({ error: 'Not a valid Credit Card Number' });
+      });
+      
+      // Test for the POST /validateCard endpoint with a valid credit card number.
+      it('should respond with 200 for POST /validateCard with a valid credit card number', async () => {
+        const response = await request(server).post('/validateCard').send({ creditCardNumber: '4556925412281287' });
         // Check that the response status is 200 (OK).
         expect(response.status).toBe(200);
         // Check that the response body contains the expected success message.
